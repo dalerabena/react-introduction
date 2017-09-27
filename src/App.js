8ce7ea618774cdc04eb2246f88acc78bae51da49
admin <admin@admin.com>
2017-09-27 15:01:58 +0800

    update

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ class App extends Component {
     return (
       <div>       
         <Click />
-        {/* <Todo /> */}
-        {/* <Giphy /> */}
+        <Todo />
+        <Giphy />
       </div>
     );
   }
